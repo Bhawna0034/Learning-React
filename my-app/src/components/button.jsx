e1ebf72fa3a4e83a11ca6af366498eb7f3537a32
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-02-11 15:53:11 +0530

    Component: Created button component

---
+++ my-app/src/components/button.jsx
@@ -0,0 +1,14 @@
+function Button(){
+    return(
+        <button>Click Me!</button>
+    )
+}
+
+export default  function MyButton(){
+    return(
+        <>
+        <h1>Welcome to my app</h1>
+        <Button/>
+        </>
+    )
+}
